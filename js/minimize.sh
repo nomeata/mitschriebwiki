547a65aa5b53d2366a82f8b4e1b9b0f6f68cb5c2
Joachim Breitner <mail@joachim-breitner.de>
2023-05-09 15:50:34 +0200

    Bump jquery

--- js/minimize.sh
+++ js/minimize.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 cd $(dirname $0)/..
 
 (
 for file in  \
-	js/jquery-1.7.2.min.js \
+	js/jquery-3.6.4.min.js \
 	js/jquery-ui-1.8.21.custom.min.js \
 	js/jquery.toc.js \
 	js/jquery.highlight.js \
@@ -14,9 +14,9 @@ for file in  \
 	js/SocialSharePrivacy/jquery.socialshareprivacy.gplus.js \
 	js/SocialSharePrivacy/jquery.socialshareprivacy.flattr.js \
 	js/latexki.js \
-	js/browserupdate.js 
+	js/browserupdate.js
 do
-	yui-compressor $file
+	yuicompressor $file
 done
 ) > js/minimized.js
 
